96342b4f1ef6409fc4f8e9f3c36e3849e2b987d8
Amalpm7 <amal.p@nestgroup.net>
2022-10-11 08:24:31 +0530

    plist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 
+const shop:Routes=[
+  {
+    path:"",component:ProductlistComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(shop)
   ],
   providers: [],
   bootstrap: [AppComponent]
